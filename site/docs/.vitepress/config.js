30c16ce1780070b47490df4c0cd9b6975e79e781
IMMYz <yz33105@gmail.com>
2023-06-30 18:02:53 +0800

    发布静态文档

---
+++ site/docs/.vitepress/config.js
@@ -0,0 +1,51 @@
+export default {
+  base: process.env.NODE_ENV === 'production' ? '/yncc-ui-doc/' : '/',
+  themeConfig: {
+    siteTitle: "vitepress",
+    nav: [
+      { text: "指南", link: "/guild/" },
+      { text: "组件", link: "/components/button/" },
+    ],
+    socialLinks: [
+      { icon: "github", link: "https://github.com/IMMYz/yncc-ui.git" },
+    ],
+    sidebar: {
+      "/guild/": [
+        {
+          text: "基础",
+          items: [
+            {
+              text: "安装",
+              link: "/guild/installation",
+            },
+            {
+              text: "快速开始",
+              link: "/guild/quickstart",
+            },
+          ],
+        },
+        {
+          text: "进阶",
+          items: [
+            {
+              text: "xx",
+              link: "/xx",
+            },
+          ],
+        },
+      ],
+      "/components/": [
+        {
+          text: "基础组件",
+          items: [
+            {
+              text: "Button",
+              link: "/components/button",
+            }
+          ],
+        }
+      ]
+    },
+
+  },
+};
---
+++ site/docs/components/button/index.md
@@ -0,0 +1,12 @@
+# button 按钮
+<cc-button>默认按钮</cc-button>
+<cc-button type="primary">默认按钮</cc-button>
+
+::: details 显示代码
+```
+<cc-button>默认按钮</cc-button>
+<cc-button type="primary">默认按钮</cc-button>
+```
+<!-- <script setup>
+  import {Button} from 'yncc'
+</script> -->
